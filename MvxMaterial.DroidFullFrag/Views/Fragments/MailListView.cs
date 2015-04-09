8c4ce569625cb3654fad93b74d053cccc0bb651d
Steff Kelsey <sk@lavjaveler.com>
2015-04-09 11:23:35 -0400

    Added action bar drawer toggle and made the drawer come in from the left (why fight it)

--- MvxMaterial.DroidFullFrag/Views/Fragments/MailListView.cs
+++ MvxMaterial.DroidFullFrag/Views/Fragments/MailListView.cs
@@ -14,6 +14,7 @@ namespace MvxMaterial.Views.Fragments
     public class MailListView : MvxFragment<MailListViewModel>
     {
         private DrawerLayout _drawerLayout;
+        private ActionBarDrawerToggle _actionBarDrawerToggle;
         private View _view;
 
         public override void OnCreate(Bundle savedInstanceState)
@@ -38,24 +39,48 @@ namespace MvxMaterial.Views.Fragments
 
             _drawerLayout = (DrawerLayout)_view.FindViewById(Resource.Id.drawer_layout);
             //drawerLayout.SetStatusBarBackgroundColor(Resource.Color.dark_orange);
+            _actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this.Activity,
+                _drawerLayout,
+                Resource.String.drawer_open,
+                Resource.String.drawer_close);
+
+            _drawerLayout.SetDrawerListener(_actionBarDrawerToggle);
 
             return _view;
         }
 
+
         public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
         {
             //Mvx.Trace("MailListView:OnCreateOptionsMenu");
-            inflater.Inflate(Resource.Menu.main, menu);
+            //inflater.Inflate(Resource.Menu.main, menu);
         }
+
 
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             Mvx.Trace("MainListView:OnOptionsItemSelected item = " + item.ToString());
-            if (item.ItemId == Resource.Id.action_settings)
+            //Mvx.Trace("itemId = " + item.ItemId);
+            //Mvx.Trace("Android.Resource.Id.Home = " + Android.Resource.Id.Home);
+            if (item != null && item.ItemId == Android.Resource.Id.Home)
             {
-                _drawerLayout.OpenDrawer(_view);
+                if (_drawerLayout.IsDrawerOpen(3))
+                {
+                    _drawerLayout.CloseDrawer(3);
+                }
+                else
+                {
+                    _drawerLayout.OpenDrawer(3);
+                }
             }
             return base.OnOptionsItemSelected(item);
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            _actionBarDrawerToggle.SyncState();
         }
     }
 }
